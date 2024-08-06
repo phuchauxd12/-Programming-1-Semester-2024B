2d434a43e02bbcb35607de0039d73ce020c08b8b
Tôn Thất Hữu Luân <s3958304@rmit.edu.vn>
2024-08-06 22:24:14 +0700

    Create Menus
    Add Welcome screen
    Update Car and Auto Part menu for RBAC in the future
    Fix exit menu logic
    Add Statistics Menu (front end)
    Add User Menu (front end)

---
+++ src/utils/UserMenu.java
@@ -0,0 +1,53 @@
+package utils;
+
+import user.User;
+
+import java.util.Scanner;
+
+import static utils.CarAndAutoPartMenu.getOption;
+
+public class UserMenu {
+    public static void menu() {
+        System.out.println("User Menu");
+        System.out.println("----------------");
+        System.out.println("1. View All Users");
+        System.out.println("2. View Users by Role");
+        System.out.println("3. View User by ID");
+        System.out.println("4. Delete User");
+        System.out.println("5. Update User");
+        System.out.println("6. Exit");
+        System.out.println("----------------");
+    }
+
+    public static void mainMenu() {
+        int option = 0;
+        Scanner input = new Scanner(System.in);
+        do {
+            menu();
+            option = getOption(option, input);
+            switch (option) {
+                case 1:
+                    User.viewAllUsers();
+                    break;
+                case 2:
+                    System.out.println("View Users by Role");
+                    break;
+                case 3:
+                    System.out.println("View User by ID");
+                    break;
+                case 4:
+                    System.out.println("Delete User");
+                    break;
+                case 5:
+                    System.out.println("Update User");
+                    break;
+                case 6:
+                    System.out.println("Exiting User Menu");
+                    break;
+                default:
+                    System.out.println("Invalid option");
+            }
+        } while (option != 6);
+        Menu.mainMenu();
+    }
+}
