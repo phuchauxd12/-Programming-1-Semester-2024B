9003fb64ec32ce66a47000852cf1f3977edc28d6
Tôn Thất Hữu Luân <s3958304@rmit.edu.vn>
2024-08-15 22:29:22 +0700

    some weird stuff idk

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import user.Client;
+import user.Mechanic;
 import user.Membership;
 import user.User;
 import utils.Menu;
+import autoPart.autoPart;
 
 import java.time.LocalDate;
-import utils.ServiceMenu;
 
 
 public class Main {
@@ -14,6 +15,9 @@ public class Main {
         Client client1 = new Client("sarah_smith", "password012", "Sarah Smith", LocalDate.of(2000, 5, 22), "012 Maple Ln", 987643210, "sarah.smith@example.com", User.ROLE.CLIENT, "Active", membership);
         Client client2 = new Client("mary_johnson", "password678", "Mary Johnson", LocalDate.of(1960, 9, 18), "678 Willow Dr", 987643210, "mary.johnson@example.com", User.ROLE.CLIENT, "Active", membership);
         Mechanic mechanic = new Mechanic("mechanic1", "password123", "John Doe", LocalDate.of(1985, 5, 15), "123 Main St", 1234567890, "john.doe@example.com", User.ROLE.EMPLOYEE, "Active", null);
+        autoPart part1 = new autoPart("Brake Pad", "ACDelco", autoPart.Condition.NEW, 12, 24.99, "Fits most Honda vehicles");
+        autoPart part2 = new autoPart("Oil Filter", "Fram", autoPart.Condition.NEW, 6, 9.99, "Compatible with various car models");
+        autoPart part3 = new autoPart("Spark Plug", "NGK", autoPart.Condition.REFURBISHED, 18, 14.99, "High-performance spark plugs");
         System.out.println("COSC2081 GROUP ASSIGNMENT");
         System.out.println("AUTO136 CAR DEALERSHIP MANAGEMENT SYSTEM");
         System.out.println("Instructor: Mr. Minh Vu & Mr. Dung Nguyen");
@@ -61,11 +65,7 @@ public class Main {
 
         /* Add to database.txt file*/
 
-        autoPart part1 = new autoPart("Brake Pad", "ACDelco", autoPart.Condition.NEW, 12, 24.99, "Fits most Honda vehicles");
 
-        autoPart part2 = new autoPart("Oil Filter", "Fram", autoPart.Condition.NEW, 6, 9.99, "Compatible with various car models");
-
-        autoPart part3 = new autoPart("Spark Plug", "NGK", autoPart.Condition.REFURBISHED, 18, 14.99, "High-performance spark plugs");
 //        AutoPartDatabase.createDatabase();
 //        AutoPartDatabase.addAutoPart(part1);
 //        AutoPartDatabase.addAutoPart(part2);
